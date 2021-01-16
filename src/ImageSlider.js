7f6f647c73b17c1f049bc06d3b13e3258d6f2875
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-16 10:12:36 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -13,7 +13,7 @@ export default class ImageSlider extends React.Component {
   render () {
     return(
       <div>
-      <p> I am on slide 0</p>
+      <p> I am on slide {this.state.currentSlideIndes} </p>
       </div>
       );
   }
