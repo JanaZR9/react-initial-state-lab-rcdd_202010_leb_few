cc7fdb290c7ba686b3947fa7e4b1ffa55b9495ed
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-16 10:18:45 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -5,7 +5,7 @@ export default class ImageSlider extends React.Component {
   constructor() {
     super ();
      this.state = {
-    currentSlideIndes:0
+    currentSlideIndex:0
   };
   
   }
@@ -13,7 +13,7 @@ export default class ImageSlider extends React.Component {
   render () {
     return(
       <div>
-      <p>I am on slide {this.state.currentSlideIndes}</p>
+      <p>I am on slide {this.state.currentSlideIndex}</p>
       </div>
       );
   }
