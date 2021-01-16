2e488d983f5d59388a3cfa4d5cf09598c99bdd54
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-16 10:13:19 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,6 +2,12 @@
 import React from 'react';
 
 export default class Bomb extends React.Component {
+  constructor (){
+    super ();
+    this.state = {
+      secondsLeft:
+    };
+  }
   render () {
     return();
   }
